30f8bce1f7d6bc0c86ee908591ec06afecc38d24
Xun Gu <yesguxun@gmail.com>
2014-11-21 17:28:22 +0900

    adding a replace in multifile command

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
+
 sudo apt-get install vim git exuberant-ctags vim-gnome
+
 cp -av bash/bash_aliases ~/.bash_aliases
 
 rm -rf ~/.vim
@@ -6,3 +9,6 @@ cp -a vim ~/
 mv ~/vim ~/.vim
 ln -s ~/.vim/vimrc ~/.vimrc
 
+mkdir -p ~/mycmd
+cp -av bash/reppp ~/mycmd/reppp
+chmod +x ~/mycmd/reppp
