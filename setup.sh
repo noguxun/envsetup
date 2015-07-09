f6c70b25407aa29837a444d1752284a12a7448f0
xgu <noguxun@gmail.com>
2015-07-09 10:34:08 +0900

    install more packages

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-sudo apt-get install vim git exuberant-ctags vim-gnome
+sudo apt-get install vim git exuberant-ctags vim-gnome unity-tweak-tool synaptic compizconfig-settings-manager
+
+#http://www.techdrivein.com/2013/03/4-simple-tweaks-to-improve-unity-performance-ubuntu.html
+sudo apt-get autoremove unity-lens-music unity-lens-photos unity-lens-shopping unity-lens-video
 
 cp -av bash/bash_aliases ~/.bash_aliases
 
